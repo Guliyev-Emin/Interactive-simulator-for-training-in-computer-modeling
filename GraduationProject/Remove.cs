7037118bb47cccfcce88e61fc1c79c89ea8e0fed
Guliyev-Emin <eminguliyev2002@gmail.com>
2022-03-06 08:38:52 +0300

    Clear code

--- GraduationProject/Remove.cs
+++ GraduationProject/Remove.cs
@@ -8,6 +8,7 @@ namespace GraduationProject
         private static Sketch _sketch;
         private static bool _boolstatus;
         private static Feature _feature;
+        private static Feature _featureForSketch;
         private static int i;
 
         public static void RemoveFeature()
@@ -15,7 +16,8 @@ namespace GraduationProject
             for (i = 0; i < _modelDoc2.GetFeatureCount() - 1; i++)
             {
                 _feature = (Feature) _modelDoc2.FeatureByPositionReverse(i);
-                
+                _featureForSketch = (Feature) _modelDoc2.FeatureByPositionReverse(i + 1);
+                swSketch = (Sketch) _featureForSketch.GetSpecificFeature2();
                 if (_feature != null)
                 {
                     MessageBox.Show(_feature.Name + @" [" + _feature.GetTypeName() + @"]");
@@ -23,21 +25,15 @@ namespace GraduationProject
                     _modelDoc2.EditDelete();
                     //Drawing._step--;
                     
-                    swSketch = (Sketch) _feature.GetSpecificFeature2();
-
                     if (swSketch != null)
                     {
                         sketchContours = (object[]) swSketch.GetSketchContours();
                         nbrSketchContours = sketchContours.Length;
 
-                        MessageBox.Show(_feature.Name + @"123234 [" + _feature.GetTypeName() + @"]");
-                        _boolstatus = _modelDoc2.Extension.SelectByID2(_feature.Name, "SKETCH", 0, 0, 0, false, 0, null, 0);
+                        MessageBox.Show(_featureForSketch.Name + @"123234 [" + _featureForSketch.GetTypeName() + @"]");
+                        _boolstatus = _modelDoc2.Extension.SelectByID2(_featureForSketch.Name, "SKETCH", 0, 0, 0, false, 0, null, 0);
                         _modelDoc2.EditDelete();
                     }
-                    
-                    
-                    
-                    
                 }
             }
         }
@@ -45,26 +41,5 @@ namespace GraduationProject
         static object[] sketchContours = null;
         static int nbrSketchContours = 0;
         static Sketch swSketch = default(Sketch);
-
-        public static void RemoveSketch()
-        {
-            for (i = 0; i < _modelDoc2.GetFeatureCount() - 1; i++)
-            {
-                //_feature = (Feature) _modelDoc2.FeatureByPositionReverse(i);
-                _feature = (Feature) _modelDoc2.FeatureByPositionReverse(i);
-
-                swSketch = (Sketch) _feature.GetSpecificFeature2();
-
-                if (swSketch != null)
-                {
-                    sketchContours = (object[]) swSketch.GetSketchContours();
-                    nbrSketchContours = sketchContours.Length;
-
-                    MessageBox.Show(_feature.Name + @" [" + _feature.GetTypeName() + @"]");
-                    _boolstatus = _modelDoc2.Extension.SelectByID2(_feature.Name, "SKETCH", 0, 0, 0, false, 0, null, 0);
-                    _modelDoc2.EditDelete();
-                }
-            }
-        }
     }
 }
